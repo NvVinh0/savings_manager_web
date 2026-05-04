9712a1019e5bf938b0507a942c6be28840e7b6cc
vietng <vietng322611@gmail.com>
2026-05-04 09:04:48 +0700

    update `SavingType` to support non-fixed-term; update `SavingAccount` and `Transaction`

--- savings/models.py
+++ savings/models.py
@@ -8,9 +8,11 @@ def generate_account_number():
 
 class SavingType(models.Model):
     name = models.CharField(max_length=50)
-    duration_months = models.IntegerField()  # 3, 6, 12
-    interest_rate = models.FloatField()
+    duration_months = models.IntegerField(null=True, blank=True)  # 3, 6, 12
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=4) # 0.05, 0.1, 0.15
     is_active = models.BooleanField(default=True)
+
+    is_flexible = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -27,10 +29,13 @@ class SavingAccount(models.Model):
     address = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
-    interest_rate = models.FloatField()  # snapshot
+
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=4) # snapshot
+    start_date = models.DateField(auto_now_add=True) # lazy evaluation
+    maturity_date = models.DateField(null=True, blank=True)  # allow null for non-fixed-term accounts
 
     saving_type = models.ForeignKey(SavingType, on_delete=models.PROTECT, related_name="accounts")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='saving_accounts')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='saving_accounts')
 
     def deposit(self, amount):
         self.balance += amount
@@ -47,15 +52,18 @@ class Transaction(models.Model):
         ('OPEN', 'Account Opening'),
         ('DEPOSIT', 'Deposit'),
         ('WITHDRAW', 'Withdrawal'),
+        ('CLOSE', 'Account Closing'),
     ]
 
     account_number = models.CharField(max_length=10)
     name = models.CharField(max_length=50)  # transaction maker's name
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    balance_before = models.DecimalField(max_digits=12, decimal_places=2)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    balance_after = models.DecimalField(max_digits=12, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    account = models.ForeignKey(SavingAccount, on_delete=models.CASCADE, related_name='transactions')
+    account = models.ForeignKey(SavingAccount, on_delete=models.SET_NULL, related_name='transactions')
 
     def __str__(self):
         return f"{self.transaction_type}: {self.amount} on {self.timestamp}"
