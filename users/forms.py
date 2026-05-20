107c2c9a187381daf537e8e3587fbb8e6e1ffca6
vietng <vietng322611@gmail.com>
2026-05-20 19:00:14 +0700

    move `forms.py` to correct location + move `EmailChangeForm` into this

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,31 @@
+from allauth.account.forms import SignupForm
 from django import forms
+from .models import CustomUser
+
+class CustomSignupForm(SignupForm):
+    full_name = forms.CharField(max_length=50)
+    citizen_id = forms.CharField(max_length=12)
+    address = forms.CharField(max_length=100)
+
+    def save(self, request):
+        user = super().save(request)
+
+        user.full_name = self.cleaned_data["full_name"]
+        user.citizen_id = self.cleaned_data["citizen_id"]
+        user.address = self.cleaned_data["address"]
+
+        user.save()
+
+        return user
+
+class InformationChangeForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "full_name",
+            "citizen_id",
+            "address",
+        ]
 
 class EmailChangeForm(forms.Form):
     email = forms.EmailField(label="New email")
