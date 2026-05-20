1baa5bac9c68ae7a3b5c4b5ced99f00fa4fd5977
vietng <vietng322611@gmail.com>
2026-05-20 18:43:11 +0700

    add validator for citizen ID

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
@@ -11,7 +12,16 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     full_name = models.CharField(max_length=50, default="")
-    citizen_id = models.CharField(max_length=12, default="", unique=True)
+    citizen_id = models.CharField(
+        max_length=12,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{12}$',
+                message='Citizen ID must contain exactly 12 digits.'
+            )
+        ]
+    )
     address = models.CharField(max_length=100, default="")
 
     USERNAME_FIELD = 'email'
