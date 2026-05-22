009c73599df8fa1a4118de26f73be9249815337f
vietng <vietng322611@gmail.com>
2026-05-22 16:41:09 +0700

    update html and view for `employees/users/`

--- employees/views.py
+++ employees/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from users.models import CustomUser
+
 
 def employee_dashboard(request):
-    pass
+    return render(request, "employees/dashboard.html")
 
 def manage_users(request):
-    pass
+    users = CustomUser.objects.all()
+    return render(request, "employees/users.html", {"users": users})
 
-def manage_staffs(request):
-    pass
+def manage_user_profile(request, user_id):
+    user = get_object_or_404(CustomUser, id=user_id)
+    return render(request, "employees/user_profile.html", {"user": user})
 
 def manage_saving_accounts(request):
     pass
