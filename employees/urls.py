ef778208c4925939361697f5d7c7f2294c94ec7e
vietng <vietng322611@gmail.com>
2026-05-22 16:41:17 +0700

    update urls

--- employees/urls.py
+++ employees/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.employee_dashboard, name="employee_dashboard"),
     path("users/", views.manage_users, name="manage_users"),
+    path("users/<int:user_id>", views.manage_user_profile, name="manage_user_profile"),
     path("savings/", views.manage_saving_accounts, name="manage_saving_accounts"),
     path("transactions/", views.manage_transactions, name="manage_transactions")
 ]
