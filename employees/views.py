1d6e3dad07e7378dfec51f4a0551a5819edfc071
vietng <vietng322611@gmail.com>
2026-05-23 16:51:20 +0700

    update form and view for `user_detail`

--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from dashboard.utils import read_session_errors
+from savings.models import SavingPlan, Transaction
+from users.forms import InformationChangeForm, EmployeeChangeForm
 from users.models import CustomUser
-
 
 def employee_dashboard(request):
     return render(request, "employees/dashboard.html")
@@ -10,12 +12,55 @@ def manage_users(request):
     users = CustomUser.objects.all()
     return render(request, "employees/users.html", {"users": users})
 
-def manage_user_profile(request, user_id):
-    user = get_object_or_404(CustomUser, id=user_id)
-    return render(request, "employees/user_profile.html", {"user": user})
-
-def manage_saving_accounts(request):
-    pass
+def manage_saving_plans(request):
+    saving_plans = SavingPlan.objects.all()
+    return render(request, "employees/savings.html", {"saving_plans": saving_plans})
 
 def manage_transactions(request):
+    transactions = Transaction.objects.all()
+    return render(request, "employees/transactions.html", {"transactions": transactions})
+
+def manage_user_detail(request, user_id):
+    user = get_object_or_404(CustomUser, id=user_id)
+
+    if request.method == "POST":
+        match request.POST.get("form_type"):
+            case "customer":
+                customer_form = InformationChangeForm(request.POST, instance=request.user.customer)
+                if customer_form.is_valid():
+                    customer_form.save()
+                    request.session["message_success"] = "Customer information updated successfully."
+                else:
+                    request.session["customer_form_errors"] = customer_form.errors
+            case "employee":
+                employee_form = EmployeeChangeForm(request.POST, instance=request.user)
+                if employee_form.is_valid():
+                    employee_form.save()
+                    request.session["message_success"] = "Customer information updated successfully."
+                else:
+                    request.session["employee_form_error"] = employee_form.errors
+
+        return redirect(request.path)
+
+    else:
+        if request.user.is_customer:
+            customer_form = InformationChangeForm(instance=request.user)
+            read_session_errors(customer_form, request.session, "customer_form_errors")
+        else:
+            customer_form = None
+
+        employee_form = EmployeeChangeForm(request.user)
+        read_session_errors(employee_form, request.session, "employee_form_errors")
+
+    return render(request, "employees/user_detail.html", {
+        "user": user,
+        "customer_form": customer_form,
+        "employee_form": employee_form,
+        "message_success": request.session.pop("message_success", None)
+    })
+
+def manage_saving_plan_detail(request, account_number):
+    pass
+
+def manage_transaction_detail(request, transaction_id):
     pass
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from allauth.account.forms import SignupForm
 from django import forms
-from .models import Customer
+from .models import Customer, EmployeeRole, Employee
+
 
 class CustomSignupForm(SignupForm):
     full_name = forms.CharField(max_length=50)
@@ -60,3 +61,51 @@ class EmailChangeForm(forms.Form):
     def save(self):
         self.user.email = self.cleaned_data["email"]
         self.user.save()
+
+class EmployeeChangeForm(forms.Form):
+    hasRead = forms.BooleanField(required=False)
+    hasWrite = forms.BooleanField(required=False)
+
+    def __init__(self, *args, user=None, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
+        if self.user.is_employee:
+            self.fields["hasRead"].initial = True
+            self.fields["hasWrite"].initial = (
+                    user.employee.role == EmployeeRole.WRITE
+            )
+
+            # Can't remove read while write exists
+            if user.employee.role == EmployeeRole.WRITE:
+                self.fields["hasRead"].disabled = True
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        has_write = cleaned_data.get("hasWrite")
+        if has_write:
+            cleaned_data["hasRead"] = True
+
+        return cleaned_data
+
+    def save(self):
+        has_read = self.cleaned_data["hasRead"]
+        has_write = self.cleaned_data["hasWrite"]
+
+        if not has_read:
+            if self.user.is_employee:
+                self.user.employee.delete()
+            return None
+
+        if has_write:
+            role = EmployeeRole.WRITE
+        else:
+            role = EmployeeRole.READ
+
+        employee, created = Employee.objects.get_or_create(user=self.user, defaults={"role": role})
+        if not created: # exist, update role
+            employee.role = role
+            employee.save()
+
+        return employee
