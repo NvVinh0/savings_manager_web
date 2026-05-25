a708850f32e09fbdc2fef2aad486edc44ce3c2be
vietng <vietng322611@gmail.com>
2026-05-25 18:56:47 +0700

    remove django admin

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', views.profile, name="profile"),
     path('', include('allauth.urls')),
--- savings/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(SavingType)
-admin.site.register(SavingPlan)
-admin.site.register(SavingTypeRateHistory)
-admin.site.register(Transaction)
