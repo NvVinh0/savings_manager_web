bdc15f4979d5663932f5ff6c9d098df3e3c77b36
vietng <vietng322611@gmail.com>
2026-05-26 12:42:45 +0700

    rename

--- employees/urls.py
+++ employees/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("users/", views.manage_users, name="manage_users"),
     path("users/<int:user_id>", views.manage_user_detail, name="manage_user_profile"),
     path("user_craete", views.user_create, name="user_create"),
-    path("savings/", views.manage_saving_plans, name="manage_saving_accounts"),
+    path("savings/", views.manage_saving_plans, name="manage_saving_plans"),
     path("transactions/", views.manage_transactions, name="manage_transactions"),
     path("reports/", views.manage_reports, name="manage_reports"),
 ]
